7eedfa7bca4a449e995113b5d9c5ac8a0e6262c2
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-09-11 20:41:53 -0300

    Criando o projeto do curso e instalando o PrimeNG

--- controle-pedidos/src/app/models/pedido.ts
+++
@@ -1,9 +0,0 @@
-export interface Pedido {
-    id: number,
-    cliente: string,
-    telefone: string,
-    valor: string,
-    pedido: string,
-    observacoes: string,
-    status?: 'PAGO' | 'PREPARANDO' | 'ENTREGUE'
-}
