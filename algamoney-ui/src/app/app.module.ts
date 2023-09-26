e1ab73bfae5b015125320742e653447c9604e073
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-09-26 20:07:27 -0300

    Adicionando caixa de seleção (componente Dropdown)

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { SelectButtonModule } from 'primeng/selectbutton';
     InputTextareaModule,
     CalendarModule,
     BrowserAnimationsModule,
-    SelectButtonModule
+    SelectButtonModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoCadastroComponent  {
 
-  tipos: any[] = [
+  tipos = [
     { label: 'Receita', value: 'RECEITA'},
     { label: 'Despesa', value: 'DESPESA'}
   ]
+
+  categorias = [
+    { label: 'Alimentação', value: 1 },
+    { label: 'Transporte', value: 2 },
+  ];
+
+  pessoas = [
+    { label: 'João da Silva', value: 4 },
+    { label: 'Sebastião Souza', value: 9 },
+    { label: 'Maria Abadia', value: 3 },
+  ];
 
 }
