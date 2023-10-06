db354b1d85b6bedaa9d0688c77ed4c1afd29a66e
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-10-05 21:04:30 -0300

    Implementando um serviço

---
+++ cadastro/cadastro/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from './funcionario.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  funcionarios: any[] = [];
+
+  funcionarioService: FuncionarioService;
+
+  constructor() {
+    this.funcionarioService = new FuncionarioService();
+  }
+
+  ngOnInit(): void {
+    this.funcionarios = this.funcionarioService.consultar();
+  }
+
+
+
+}
---
+++ cadastro/cadastro/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuncionarioCardComponent,
+    FuncionarioFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
