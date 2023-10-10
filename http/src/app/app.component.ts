87c209cd2951e9f2ef25ed326539b667d1b7e9e8
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-10-09 21:14:43 -0300

    Fazendo requisição com GET e recebendo o retorno

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { CidadeService } from './cidade.service';
+import { Component, OnInit } from '@angular/core';
 
+interface Cidade {
+  id: number,
+  nome: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  cidades: Cidade[] = [];
+ 
+  constructor(private cidadeService: CidadeService) {}
+ 
+  ngOnInit(): void {
+     this.cidadeService.consultar()
+        .then(dados => this.cidades = dados);
+  }
   
-  cidades = [
-    { id: 1, nome: 'Uberlândia' },
-    { id: 2, nome: 'São Paulo' },
-    { id: 3, nome: 'Florianópolis' },
-    { id: 4, nome: 'Curitiba' }
-  ];
+
 
   adicionar(nome: string) {
     alert(nome);
--- http/src/app/app.module.ts
+++ http/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CidadeService } from './cidade.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CidadeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
