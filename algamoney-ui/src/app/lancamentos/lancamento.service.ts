8c6fae037eabee8534b831c263ee452141874ea7
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-10-12 17:14:44 -0300

    Implementando a paginação no serviço de lançamentos

--- algamoney-ui/src/app/lancamentos/lancamento.service.ts
+++ algamoney-ui/src/app/lancamentos/lancamento.service.ts
@@ -3,10 +3,12 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
-export interface LancamentoFiltro {
-  descricao: string;
+export class LancamentoFiltro {
+  descricao!: string;
   dataVencimentoInicio?: Date;
   dataVencimentoFim?: Date;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 @Injectable({
   providedIn: 'root'
@@ -23,6 +25,9 @@ export class LancamentoService {
 
     let params = new HttpParams();
 
+    params = params.set('page', filtro.pagina);
+    params = params.set('size', filtro.itensPorPagina);
+
     if(filtro.descricao) {
         params = params.set('descricao', filtro.descricao)
     }
@@ -37,6 +42,15 @@ export class LancamentoService {
 
      return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
         .toPromise()
-        .then((response: any) => response.content);
+        .then((response: any) =>  {
+            const lancamentos = response.content;
+
+            const resultado = {
+                lancamentos,
+                total: response.totalElements
+            };
+
+            return resultado;
+        });
   }
 }
