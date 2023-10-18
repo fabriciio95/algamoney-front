8973f584ab075924d525656e6d24c24e06fce01a
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-10-18 20:01:46 -0300

    Listando as categorias cadastradas no dropdown

--- algamoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 
 @Component({
   selector: 'app-lancamento-cadastro',
   templateUrl: './lancamento-cadastro.component.html',
   styleUrls: ['./lancamento-cadastro.component.css']
 })
-export class LancamentoCadastroComponent  {
+export class LancamentoCadastroComponent implements OnInit {
 
   tipos = [
     { label: 'Receita', value: 'RECEITA'},
     { label: 'Despesa', value: 'DESPESA'}
   ]
 
-  categorias = [
-    { label: 'Alimentação', value: 1 },
-    { label: 'Transporte', value: 2 },
-  ];
+  categorias = [];
 
   pessoas = [
     { label: 'João da Silva', value: 4 },
     { label: 'Sebastião Souza', value: 9 },
     { label: 'Maria Abadia', value: 3 },
   ];
+
+  constructor(private categoriaService: CategoriaService,
+              private errorHandler: ErrorHandlerService) { }
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+  }
+
+
+  carregarCategorias() {
+    this.categoriaService.listarTodas()
+       .then(categorias => {
+            this.categorias = categorias.map((c: any) =>  ({ label: c.nome, value: c.codigo } ));
+       }).catch(error => this.errorHandler.handle(error));
+  }
 
 }
