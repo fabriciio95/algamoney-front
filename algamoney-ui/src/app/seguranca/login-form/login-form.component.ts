b76adcd8e4d09115291e0348aabf7015ada6320c
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-11-20 19:29:12 -0300

    Criando o módulo Dashboard

--- algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
+++ algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
@@ -16,7 +16,7 @@ export class LoginFormComponent {
 
   login(usuario: string, senha: string) {
       this.authService.login(usuario, senha)
-           .then(() => this.router.navigate(['/lancamentos']))
+           .then(() => this.router.navigate(['/dashboard']))
             .catch(erro => this.errorHandler.handle(erro));
 
   }
