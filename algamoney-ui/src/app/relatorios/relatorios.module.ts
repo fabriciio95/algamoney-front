ab3a2d65f89bf2b2fb803a09b93f7fb28f702844
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-11-21 20:20:52 -0300

    Criando módulo de relatórios

---
+++ algamoney-ui/src/app/relatorios/relatorios.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RelatoriosRoutingModule } from './relatorios-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
+
+
+@NgModule({
+  declarations: [
+    RelatorioLancamentosComponent
+  ],
+  imports: [
+    CommonModule,
+    RelatoriosRoutingModule,
+    SharedModule
+  ]
+})
+export class RelatoriosModule { }
